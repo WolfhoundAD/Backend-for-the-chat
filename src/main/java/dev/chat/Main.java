b2460ac932a2f459843d40818cca2ea8c3d810af
WolfhoundAD <Loners14@yandex.ru>
2024-05-09 13:23:01 +0300

    Add a connection to minIO

--- src/main/java/dev/chat/Main.java
+++ src/main/java/dev/chat/Main.java
@@ -1,9 +1,15 @@
 package dev.chat;
 
 import dev.chat.service.Initializer;
+import io.minio.MinioClient;
+import io.minio.errors.MinioException;
+import io.minio.messages.Bucket;
+import org.apache.maven.plugin.lifecycle.Execution;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -11,5 +17,23 @@ public class Main {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
         Initializer initializer = context.getBean(Initializer.class);
         initializer.initialize();
+
+        MinioClient minioClient = demo();
+        try{
+            List<Bucket> bList = minioClient.listBuckets();
+            System.out.println("Connection success, total buskets: " + bList.size());
+        } catch (MinioException e){
+            System.out.println("Connection faild: " + e.getMessage());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static MinioClient demo(){
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint("http://127.0.0.1:9000")
+                .credentials("minioadmin", "minioadmin")
+                .build();
+        return minioClient;
     }
 }
