5da58697c24bf7034e8e126bc768988cd631cde0
WolfhoundAD <Loners14@yandex.ru>
2024-06-15 19:05:15 +0300

    Remake controller auth

--- src/main/java/dev/chat/config/SecurityConfig.java
+++ src/main/java/dev/chat/config/SecurityConfig.java
@@ -3,13 +3,17 @@ package dev.chat.config;
 import dev.chat.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login", "/register", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated()
@@ -47,4 +51,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login?logout")
                 .permitAll();
     }
+     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 }
--- src/main/java/dev/chat/controller/v1/AuthController.java
+++ src/main/java/dev/chat/controller/v1/AuthController.java
@@ -1,37 +1,56 @@
 package dev.chat.controller.v1;
 
 import dev.chat.dto.UserDTO;
+import dev.chat.service.CustomUserDetailsService;
 import dev.chat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("")
 public class AuthController {
 
     @Autowired
     private UserService userService;
 
     @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/login")
-    public String showLoginPage() {
-        return "login";
-    }
-
-    @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("user", new UserDTO());
-        return "register";
-    }
-
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute("user") UserDTO userDTO) {
+    public Map<String, String> registerUser(@RequestBody UserDTO userDTO) {
         userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         userService.createUser(userDTO);
-        return "redirect:/login";
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "User registered successfully");
+        return response;
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> loginUser(@RequestBody UserDTO userDTO, HttpServletRequest request) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(userDTO.getUsername());
+        if (userDetails != null && passwordEncoder.matches(userDTO.getPassword(), userDetails.getPassword())) {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "User logged in successfully");
+            return response;
+        } else {
+            throw new RuntimeException("Invalid username or password");
+        }
     }
 }
